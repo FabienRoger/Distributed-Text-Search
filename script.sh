fbc2ed404b55fd68e3453aa7d1395b6315cf9b24
Raphael <raphael.habsieger@polytechnique.edu>
2023-03-08 17:52:48 +0100

    working version

--- script.sh
+++ script.sh
@@ -8,4 +8,4 @@ nvcc -I. -c src/flexible_mpi.cu -o out/flexible_mpi_cu
 mpicc -fopenmp out/flexible_mpi out/flexible_mpi_cu -lcudart -L/usr/local/cuda/lib64 -o out/flexible_mpi_exec
 # THREAD_PER_BLOCK=1 BLOCK_PER_GRID=1 USE_GPU=1 salloc -N 2 -n 8 mpirun out/flexible_exec 1 obj AAA AAAAA
 
-USE_GPU=0 DISTRIBUTE_PATTERNS=1 salloc -N 2 -n 8 mpirun /users/eleves-b/2020/fabien.roger/inf560/apm/out/flexible_mpi_exec 1 /users/eleves-b/2020/fabien.roger/inf560/apm/obj CUTH Q Q Q Q
+USE_GPU=0 DISTRIBUTE_PATTERNS=1 salloc -N 2 -n 8 mpirun /users/eleves-a/2020/$(whoami)/TD/INF560/Distributed-Text-Search/out/flexible_mpi_exec 1 /users/eleves-a/2020/$(whoami)/TD/INF560/Distributed-Text-Search/obj CUTH Q Q Q Q
--- src/flexible_mpi.cu
+++ src/flexible_mpi.cu
@@ -79,7 +79,7 @@ int required_mem(int length)
     return (length + 1) * sizeof(int);
 }
 
-extern "C" void compute_matches_gpu(char *buf, int start, int end, int n_bytes, char **patterns, int starti, int endi, int approx_factor, int max_pattern_length, int *n_matches)
+extern "C" void compute_matches_gpu(char *buf, int start, int end, int n_bytes, char **patterns, int starti, int endi, int approx_factor, int max_pattern_length, int *n_matches, int end_data)
 {
 
     if (gpu_initialized == 0)
@@ -103,9 +103,9 @@ extern "C" void compute_matches_gpu(char *buf, int start, int end, int n_bytes, 
     int *d_n_matches;
 
     cudaSetDevice(0);
-    cudaMalloc((void **)&d_buf, sizeof(char) * n_bytes);
+    cudaMalloc((void **)&d_buf, sizeof(char) * (end_data - start));
     cudaMalloc((void **)&d_n_matches, sizeof(int) * endi);
-    cudaMemcpy(d_buf, buf, sizeof(char) * n_bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_buf, buf, sizeof(char) * (end_data - start), cudaMemcpyHostToDevice);
     cudaMemcpy(d_n_matches, n_matches, sizeof(int) * endi, cudaMemcpyHostToDevice);
 
     /* Traverse the patterns */
