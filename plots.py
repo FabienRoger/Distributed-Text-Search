801108d18082b54f04ece496a346a4ed80d31e4a
Fabien Roger <fabien.roger@ain.polytechnique.fr>
2023-03-13 23:34:34 +0100

    Final plots and measurements

--- plots.py
+++ plots.py
@@ -36,9 +36,9 @@ for name in scenario_names:
             thread_per_block_values == row["thread_per_block"],
             block_per_grid_values == row["block_per_grid"],
         ] = row["mean_time"]
-    plt.imshow(heatmap, vmin=0.37, vmax=0.5)
+    plt.imshow(heatmap, vmin=sub_df["mean_time"].min(), vmax=0.5)
     plt.colorbar()
-    plt.xticks(range(len(block_per_grid_values)), block_per_grid_values)
+    plt.xticks(range(len(block_per_grid_values)), block_per_grid_values, rotation=90)
     plt.xlabel("block_per_grid")
     plt.yticks(range(len(thread_per_block_values)), thread_per_block_values)
     plt.ylabel("thread_per_block")
@@ -97,20 +97,24 @@ for i, setting in enumerate(settings):  # type: ignore
         label=setting,
     )
 plt.ylabel("scenario")
-plt.yticks(ys, scenarios)
+plt.yticks(ys, scenarios) # type: ignore
 plt.xlabel("mean execution time [s]")
 plt.legend()
 # %%
 ### Weak scaling
 # with open("results_strong_scaling_v4.csv", "w") as f:
 #     f.write("scenario,nodes,mean_time,std_time\n")
-results_strong = pd.read_csv("results_weak_scaling_v4.csv")
+results_weak = pd.read_csv("results_weak_scaling_v4.csv")
 for name in scenario_names:
-    y = results_strong[results_strong["scenario"] == name]["mean_time"]
-    yerr = results_strong[results_strong["scenario"] == name]["std_time"]
-    x = results_strong[results_strong["scenario"] == name]["nodes"]
+    y = results_weak[results_weak["scenario"] == name]["mean_time"]
+    yerr = results_weak[results_weak["scenario"] == name]["std_time"]
+    x = results_weak[results_weak["scenario"] == name]["nodes"]
     plt.errorbar(x, y, yerr=yerr, label=name)
-plt.legend()
-plt.xscale("log")
+# plt.xscale("log")
+plt.ylim(0, results_weak["mean_time"].max() * 1.1)
 plt.ylabel("mean execution time [s]")
+first_point = results_weak[results_weak["nodes"] == 1]["mean_time"].mean()
+plt.plot([1, results_weak["nodes"].max()], [first_point, first_point * results_weak["nodes"].max()], "--", label="linear scaling")
+plt.legend()
+plt.xlabel("number of nodes")
 # %%
